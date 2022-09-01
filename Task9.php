43552505de0cb68ba42e881e23aeb82950248a1c
vitaliysheshkoff <vitaliysheshkoff@gmail.com>
2022-09-02 00:19:58 +0300

    init commit

--- Task9.php
+++ Task9.php
@@ -4,15 +4,22 @@ namespace src;
 
 class Task9
 {
-    public function main(array $arr, int $number): string
+    public function main(int $input): string
     {
-        $len = count($arr);
-        $res = [];
+        if ($input < 1) {
+            throw new \InvalidArgumentException();
+        }
 
-        for ($i = 0; $i < $len - 2; $i++) {
-            if (($arr[$i] + $arr[$i + 1] + $arr[$i + 2]) == $number) {
-                $res[] = "{$arr[$i]} + {$arr[$i + 2]} + {$arr[$i + 2]} = {$number}";
+        $res = [$input];
+
+        while ($input > 1) {
+            if ($input % 2 == 0) {
+                $input /= 2;
+            } else {
+                $input = $input * 3 + 1;
             }
+
+            $res[] = $input;
         }
 
         return print_r($res, true);
