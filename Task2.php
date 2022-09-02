5793819c24e65849621e7338510c99440b3cd523
vitaliysheshkoff <vitaliysheshkoff@gmail.com>
2022-09-02 13:57:00 +0300

    init commit

--- Task2.php
+++ Task2.php
@@ -4,16 +4,18 @@ namespace src;
 
 class Task2
 {
-    public const SECONDS_PER_DAY = 86400;
-
     public function main(string $date): int
     {
-        $input = \DateTime::createFromFormat('j.m.Y', $date);
+        $input = \DateTime::createFromFormat('j-m-Y', $date);
 
-        if ($input !== false) {
-            return (int)(time() - strtotime($date)) / self::SECONDS_PER_DAY;
+        if ($input === false) {
+            throw new \InvalidArgumentException();
         }
 
-        throw new \InvalidArgumentException();
+        $current = new \DateTime('now');
+
+        $answer = $input->diff($current);
+
+        return $answer->days;
     }
 }
