9993d51863fde2399af426979e4afc131b940d92
vitaliysheshkoff <vitaliysheshkoff@gmail.com>
2022-09-02 12:43:55 +0300

    init commit

--- Task5.php
+++ Task5.php
@@ -4,21 +4,49 @@ namespace src;
 
 class Task5
 {
-    public function main(int $n): float
+    public function main(int $n): string
     {
         $previous = 0;
         $next = 1;
         $result = 0;
 
         while (strlen($result) < $n) {
-            $result = $previous + $next;
+            $result = $this->add($previous, $next);
             $previous = $next;
             $next = $result;
         }
 
         return $result;
     }
-}
 
-$res = new Task5;
-echo $res->main(20);
+    public function add(string $a, string $b): string
+    {
+        $x = str_split($a);
+        $y = str_split($b);
+
+        $tmp = 0;
+
+        $result = [];
+
+        while (count($x) || count($y)) {
+            $iterResult = array_pop($x) + array_pop($y) + $tmp;
+
+            $stringIterResult = (string)$iterResult;
+
+            if ($iterResult >= 10) {
+                $tmp = 1;
+                $stringIterResult = substr($stringIterResult, -1);
+            } else {
+                $tmp = 0;
+            }
+
+            array_unshift($result, $stringIterResult);
+        }
+
+        if ($tmp) {
+            array_unshift($result, '1');
+        }
+
+        return ltrim(implode($result));
+    }
+}
