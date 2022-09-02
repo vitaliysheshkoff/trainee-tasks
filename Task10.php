3a8d3637e5ab67e2973c80b3c7324289a3e1eac0
vitaliysheshkoff <vitaliysheshkoff@gmail.com>
2022-09-02 08:40:21 +0300

    rename class

---
+++ Task10.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace src;
+
+class Task10
+{
+    public function main(int $input): string
+    {
+        if ($input < 1) {
+            throw new \InvalidArgumentException();
+        }
+
+        $result = [$input];
+
+        while ($input > 1) {
+            if ($input % 2 == 0) {
+                $input /= 2;
+            } else {
+                $input = $input * 3 + 1;
+            }
+
+            $result[] = $input;
+        }
+
+        return print_r($result, true);
+    }
+}
