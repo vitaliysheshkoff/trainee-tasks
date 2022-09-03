b4b797a770cec83c43738f298ee471754dde0926
vitaliysheshkoff <vitaliysheshkoff@gmail.com>
2022-09-03 14:19:08 +0300

    added input checks

--- Task8.php
+++ Task8.php
@@ -2,13 +2,23 @@
 
 namespace src;
 
+
+
 class Task8
 {
     public function main(string $json): string
     {
+
+        if(is_numeric($json))
+            throw new \InvalidArgumentException();
+
         $obj = json_decode($json, true);
 
-        if ($obj === null && json_last_error() !== JSON_ERROR_NONE) {
+       /* if(!is_object($obj) || json_last_error() !== JSON_ERROR_NONE){
+            throw new \InvalidArgumentException();
+        }*/
+
+        if ($obj === null || json_last_error() !== JSON_ERROR_NONE) {
             throw new \InvalidArgumentException();
         }
 
@@ -21,3 +31,6 @@ class Task8
         return rtrim($str);
     }
 }
+
+$task = new Task8();
+echo $task->main(-1);
