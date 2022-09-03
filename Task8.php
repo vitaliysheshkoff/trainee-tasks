18472995ee2f82cee17645ee858c1446b2a45414
vitaliysheshkoff <vitaliysheshkoff@gmail.com>
2022-09-03 18:23:31 +0300

    changed PHP.EOL to delim: "\r\n"

--- Task8.php
+++ Task8.php
@@ -2,21 +2,15 @@
 
 namespace src;
 
-
-
 class Task8
 {
     public function main(string $json): string
     {
-
-        if(is_numeric($json))
+        if (is_numeric($json)) {
             throw new \InvalidArgumentException();
+        }
 
         $obj = json_decode($json, true);
-
-       /* if(!is_object($obj) || json_last_error() !== JSON_ERROR_NONE){
-            throw new \InvalidArgumentException();
-        }*/
 
         if ($obj === null || json_last_error() !== JSON_ERROR_NONE) {
             throw new \InvalidArgumentException();
@@ -25,12 +19,9 @@ class Task8
         $str = '';
 
         array_walk_recursive($obj, function ($value, $key) use (&$str) {
-            $str .= $key . ': ' . $value . PHP_EOL;
+            $str .= $key . ': ' . $value . "\r\n";
         });
 
         return rtrim($str);
     }
 }
-
-$task = new Task8();
-echo $task->main(-1);
