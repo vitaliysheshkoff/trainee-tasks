97c6197a7655e210edec2df94bb41c6810a9a497
vitaliysheshkoff <vitaliysheshkoff@gmail.com>
2022-09-03 17:43:21 +0300

    added input checks

--- Task9.php
+++ Task9.php
@@ -6,6 +6,10 @@ class Task9
 {
     public function main(array $arr, int $number): string
     {
+        /*if (count($arr) < 3 || $number <= 0 || !$this->checkISArrayPositive($arr)) {
+            throw new \InvalidArgumentException();
+        }*/
+
         $len = count($arr);
         $res = [];
 
@@ -13,6 +17,10 @@ class Task9
             if (($arr[$i] + $arr[$i + 1] + $arr[$i + 2]) == $number) {
                 $res[] = "{$arr[$i]} + {$arr[$i + 1]} + {$arr[$i + 2]} = {$number}";
             }
+        }
+
+        if (count($res) == 0) {
+            throw new \InvalidArgumentException();
         }
 
         return print_r($res, true);
