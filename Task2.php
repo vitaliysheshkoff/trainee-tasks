a5e3b41f102777ddbaa2f5e0575307b72a8eb327
vitaliysheshkoff <vitaliysheshkoff@gmail.com>
2022-09-04 00:57:57 +0300

    changed the solution

--- Task2.php
+++ Task2.php
@@ -6,16 +6,33 @@ class Task2
 {
     public function main(string $date): int
     {
-        $input = \DateTime::createFromFormat('j-m-Y', $date);
+        $input = \DateTime::createFromFormat('d-m-Y', $date);
+        $errors = \DateTime::getLastErrors();
 
-        if ($input === false) {
+        if ($input === false || !empty($errors['warning_count'])) {
             throw new \InvalidArgumentException();
         }
 
-        $current = new \DateTime('now');
+        $current = new \DateTime('today' /*'now'*/);
 
-        $answer = $input->diff($current);
+        $inputWithCurrentYear = date_create($input->format($current->format('Y') . '-m-d'));
 
-        return $answer->days;
+        // $dateInterval = $current->diff($input);
+        $dateInterval = $current->diff($inputWithCurrentYear);
+
+        $daysDiff = (int)$dateInterval->format('%R%a');
+
+        // for input date with current year
+        $leapYear = (bool)$current->format('L');
+
+        if ($daysDiff < 0) {
+            if ($leapYear) {
+                $daysDiff += 366;
+            } else {
+                $daysDiff += 365;
+            }
+        }
+        //
+        return $daysDiff;
     }
 }
